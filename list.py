1b9b55af39c259817f5433ed90698c71367393e4
shanilts <shanilts123@gmail.com>
2019-09-18 15:55:09 +0530

    new pro

--- list.py
+++ list.py
@@ -1,8 +1,11 @@
-a=["d","6",1232]
-b={"hai","ahsdjksahd"}
+a=["d","6",1232] #list
+b={"hai","ahsdjksahd"} #set
+c=("hai") #tuple
+s={"hai":1,"adsa":5}
 for i in a:
     if i=="6":
        print("yes")
 for i in b:
     if i=="hai":
         print("found")
+print(s["hai"])
